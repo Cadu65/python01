5a8e71aaaed9c4291d415a1dc7bd0a47ee8e2e25
Cadu65 <carlinhos2jeeh@gmail.com>
2024-10-14 11:57:36 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+numero1 = float(input("Digite o primeiro numero:"))
+numero2 = float(input("Digite o segundo numero:"))
+
+soma = numero1 + numero2
+subtração = numero1 - numero2
+divisão = numero1 / numero2
+multiplicação = numero1 * numero2
+
+
+print("Soma: ", soma)
+print("Subtração: ", subtração)
+print("Divisão: ", divisão)
+print("Multiplicação: ", multiplicação)
+
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
